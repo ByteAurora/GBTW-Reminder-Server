587ff748ef5d2cbd5db6c7d38e6fa8c797016a9f
Sohn Young Jin <sonkim1001@naver.com>
2021-07-13 16:02:12 +0900

    Add gbtw_server.py - Main source
    Add update_parser.py
    - Parsing from Gunship Battle : Total Warfare updates
    - Link: https://community.joycity.com/gw/update

--- gbtw_server.py
+++ gbtw_server.py
@@ -1,33 +1,39 @@
 from pyfcm import FCMNotification
 
-from pyfcm import FCMNotification
+APIKEY = "AAAAeMqKdfs:APA91bGdm7YD_RVcTX9_1oiJy_S4rZEgHmu66d" \
+         "mbZZRaiRvpGD_kYL0kR7amy2o-I0XhEUxw_cOyoUDMvlncNhc0" \
+         "X16Y-xGb3-B_Dq1LIr8fpEKk9DuvR-KhvKm4B1ymvpYDGnnoDj6i"
+TOKEN = "d1VRBeqsSJiyR6djvMqrNH:APA91bHojG43VZL3M" \
+        "9PTp6T_RLLWpcQocPBLB4_Rog_7cQuN1sF8-3t0j" \
+        "TTsD-qI16PSu5HbILXFBg6eY7TFP5dCpYdS4Y4tW" \
+        "ehcRYgN2z29-5NU64KuiNqtt99gBGAeuTkVwaOfMJ62"
 
-APIKEY = "AAAA2wElNj0:APA91bHEaiA0Sv_2fm"\
-         "t0d7fW7P6fdXNr1yR9Tz0loDKF6TI45"\
-         "nQUHrXLvMIyU08gOM2JXwQBogn0"\
-         "KaQc0pTAbFw1-xZyLR7DWGcebHLi"\
-         "--0Cv6G0ZAdxHaw5CpQLdqg_jqQ"\
-         "bsdgM6nBR"
-TOKEN = "Your Token"
-
-# 파이어베이스 콘솔에서 얻어 온 서버 키를 넣어 줌
 push_service = FCMNotification(APIKEY)
 
+print(APIKEY)
+print(TOKEN)
 
-def sendMessage(body, title):
+
+def send_message(body, title):
     # 메시지 (data 타입)
     data_message = {
         "body": body,
         "title": title
     }
 
-    # 토큰값을 이용해 1명에게 푸시알림을 전송함
-    result = push_service.single_device_data_message(registration_id=TOKEN, data_message=data_message)
+    # 데이터 형식으로 전달
+    #result = push_service.single_device_data_message(registration_id=TOKEN, data_message=data_message)
 
-    # 전송 결과 출력
-    print(result)
+    # 알림 형식으로 전달
+    #result = push_service.notify_single_device(registration_id=TOKEN, message_title=title, message_body=body)
 
-sendMessage("배달의 민족", "치킨 8000원 쿠폰 도착!")
+    # 그룹 전송
+    result = push_service.notify_topic_subscribers(topic_name="GBTF", message_title=title, message_body=body)
+
+    print("결과: ", result)
+
+
+send_message("GBTW", "Test Message")
 
 '''
 APIKEY = "AAAA2wElNj0:APA91bHEaiA0Sv_2fm"\
@@ -47,7 +53,7 @@ def send_message(title, message):
     }
 
     result = push_service.notify_topic_subscribers(topic_name="GBTF", data_message=data_message)
-    print('결과', result)
+    print("결과: ", result)
 
 
 send_message("GBTF Reminder", "반갑습니다")
