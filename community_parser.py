07a697b8345bbd86ffcd1cfa4fa012998f797b84
Sohn Young Jin <sonkim1001@naver.com>
2021-07-13 19:41:34 +0900

    Add send_message_to_group - For subscribe topic: notice, update etc..
    Add youtube upload notification

--- community_parser.py
+++ community_parser.py
@@ -35,8 +35,7 @@ def check_community():
 
             if item not in before_notice_list:
                 print('공지사항', element.text.strip())
-                print(sender.send_notify_to_all('공지사항', element.text.strip()))
-
+                print(sender.send_notify_to_group('notice', '공지사항', element.text.strip()))
         before_notice_list = new_notice_list
 
         # 신규 업데이트 확인
@@ -53,25 +52,24 @@ def check_community():
                 if '업데이트' in item.title:
                     if '완료' in item.title:
                         print('업데이트 완료: ', element.text.strip())
-                        print(sender.send_notify_to_all('업데이트 완료', element.text.strip()))
+                        print(sender.send_notify_to_group('update', '업데이트 완료', element.text.strip()))
                     else:
                         print('업데이트 예정: ', element.text.strip())
-                        print(sender.send_notify_to_all('신규 업데이트 공지', element.text.strip()))
+                        print(sender.send_notify_to_group('update', '신규 업데이트 공지', element.text.strip()))
                 elif '정기' in item.title:
                     if '완료' in item.title:
                         print('업데이트 완료: ', element.text.strip())
-                        print(sender.send_notify_to_all('업데이트 완료', element.text.strip()))
+                        print(sender.send_notify_to_group('update', '업데이트 완료', element.text.strip()))
                     else:
                         print('업데이트 예정: ', element.text.strip())
-                        print(sender.send_notify_to_all('정기점검 공지', element.text.strip()))
+                        print(sender.send_notify_to_group('update', '정기점검 공지', element.text.strip()))
                 elif '임시' in item.title:
                     if '완료' in item.title:
                         print('임시점검 완료: ', element.text.strip())
-                        print(sender.send_notify_to_all('임시점검 완료', element.text.strip()))
+                        print(sender.send_notify_to_group('update', '임시점검 완료', element.text.strip()))
                     else:
                         print('임시점검 예정: ', element.text.strip())
-                        print(sender.send_notify_to_all('긴급 공지: 임시점검', element.text.strip()))
-
+                        print(sender.send_notify_to_group('update', '긴급 공지: 임시점검', element.text.strip()))
         before_update_list = new_update_list
 
         # 신규 이벤트 확인
@@ -87,14 +85,13 @@ def check_community():
             if item not in before_event_list:
                 if '쿠폰' in item.title:
                     print('이벤트(쿠폰): ', element.text.strip())
-                    print(sender.send_notify_to_all('신규 이벤트 공지(쿠폰)', element.text.strip()))
+                    print(sender.send_notify_to_group('event', '신규 이벤트 공지(쿠폰)', element.text.strip()))
                 elif '원스토어' in item.title:
                     print('이벤트(원스토어): ', element.text.strip())
-                    print(sender.send_notify_to_all('원스토어 이벤트', element.text.strip()))
+                    print(sender.send_notify_to_group('event', '원스토어 이벤트', element.text.strip()))
                 else:
                     print('이벤트: ', element.text.strip())
-                    print(sender.send_notify_to_all('신규 이벤트 공지', element.text.strip()))
-
+                    print(sender.send_notify_to_group('event', '신규 이벤트 공지', element.text.strip()))
         before_event_list = new_event_list
 
         # 종료 이벤트 확인
@@ -109,8 +106,7 @@ def check_community():
 
             if item not in before_end_event_list:
                 print('이벤트 종료: ', element.text.strip())
-                print(sender.send_notify_to_all('이벤트 종료', element.text.strip()))
-
+                print(sender.send_notify_to_group('endEvent', '이벤트 종료', element.text.strip()))
         before_end_event_list = new_end_event_list
 
         # 이벤트 당첨자 발표
@@ -125,8 +121,16 @@ def check_community():
 
             if item not in before_event_winner_list:
                 print('이벤트 당첨자 발표: ', element.text.strip())
-                print(sender.send_notify_to_all('이벤트 당첨자 발표', element.text.strip()))
-
+                print(sender.send_notify_to_group('eventWinner', '이벤트 당첨자 발표', element.text.strip()))
         before_event_winner_list = new_event_winner_list
+
+        # 유튜브 신규 영상
+        page = requests.get("https://www.youtube.com/c/jcenest/videos")
+        soup = bs(page.text, "html.parser")
+        elements = soup.select('a.yt-simple-endpoint')
+
+        for index, element in enumerate(reversed(elements), 1):
+            print('유튜브 신규 영상', element.text.strip())
+            print(sender.send_notify_to_group('youtube', '유튜브 신규 영상', element.text.strip()))
 
         time.sleep(5)
--- send_notification.py
+++ send_notification.py
@@ -7,9 +7,14 @@ APIKEY = "AAAAeMqKdfs:APA91bGdm7YD_RVcTX9_1oiJy_S4rZEgHmu66d" \
 push_service = FCMNotification(APIKEY)
 
 
-# 공지 전송 - 그룹
+# 공지 전송 - 전체
 def send_notify_to_all(title, message):
     return push_service.notify_topic_subscribers(topic_name="GBTF", message_title=title, message_body=message)
+
+
+# 공지 전송 - 그룹
+def send_notify_to_group(topic_name, title, message):
+    return push_service.notify_topic_subscribers(topic_name=topic_name, message_title=title, message_body=message)
 
 
 # 공지 전송 - 개인
@@ -17,13 +22,22 @@ def send_notify_to_target(token, title, message):
     return push_service.notify_single_device(registration_id=token, message_title=title, message_body=message)
 
 
-# 데이터 전송 - 그룹
+# 데이터 전송 - 전체
 def send_data_to_all(title, message):
     data_message = {
         "title": title,
         "body": message
     }
     return push_service.topic_subscribers_data_message(topic_name="GBTF", data_message=data_message)
+
+
+# 데이터 전송 - 그룹
+def send_data_to_group(topic_name, title, message):
+    data_message = {
+        "title": title,
+        "body": message
+    }
+    return push_service.topic_subscribers_data_message(topic_name=topic_name, data_message=data_message)
 
 
 # 데이터 전송 - 개인
