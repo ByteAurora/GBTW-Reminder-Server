04e7ca26f69fc22cbed612d56ab429c7b7a6eb54
Sohn Young Jin <sonkim1001@naver.com>
2021-07-14 14:05:33 +0900

    Update kor -> eng

--- community_parser.py
+++ community_parser.py
@@ -39,7 +39,7 @@ def check_community():
 
         driver.get('https://www.youtube.com/user/jcenest/videos')
 
-        # 신규 공지, 쿠폰 확인
+        # Check new notice, coupon
         page = requests.get("https://community.joycity.com/gw/notice")
         soup = bs(page.text, "html.parser")
         elements = soup.select('div.sub_title a')
@@ -50,18 +50,18 @@ def check_community():
             if '쿠폰' not in title:
                 new_notice_list.append(title)
                 if title not in before_notice_list:
-                    print('공지사항', element.text.strip())
+                    print('notice', element.text.strip())
                     print(sender.send_notify_to_group('notice', '공지사항', element.text.strip()))
             else:
                 new_notice_coupon_list.append(title)
                 if title not in before_notice_coupon_list:
-                    print('쿠폰', element.text.strip())
-                    print(sender.send_notify_to_group('noticecoupon', '쿠폰', element.text.strip()))
+                    print('coupon', element.text.strip())
+                    print(sender.send_notify_to_group('noticeCoupon', '쿠폰', element.text.strip()))
 
         before_notice_list = copy.deepcopy(new_notice_list)
         before_notice_coupon_list = copy.deepcopy(new_notice_coupon_list)
 
-        # 신규 업데이트 확인
+        # Check new update
         page = requests.get("https://community.joycity.com/gw/update")
         soup = bs(page.text, "html.parser")
         elements = soup.select('div.sub_title a')
@@ -74,28 +74,28 @@ def check_community():
             if title not in before_update_list:
                 if '업데이트' in title:
                     if '완료' in title:
-                        print('업데이트 완료: ', element.text.strip())
+                        print('(END)Update: ', element.text.strip())
                         print(sender.send_notify_to_group('update', '업데이트 완료', element.text.strip()))
                     else:
-                        print('업데이트 예정: ', element.text.strip())
+                        print('Update: ', element.text.strip())
                         print(sender.send_notify_to_group('update', '신규 업데이트 공지', element.text.strip()))
                 elif '정기' in title:
                     if '완료' in title:
-                        print('업데이트 완료: ', element.text.strip())
-                        print(sender.send_notify_to_group('update', '업데이트 완료', element.text.strip()))
+                        print('(END)Periodic inspection: ', element.text.strip())
+                        print(sender.send_notify_to_group('update', '정기점검 완료', element.text.strip()))
                     else:
-                        print('업데이트 예정: ', element.text.strip())
+                        print('Periodic inspection: ', element.text.strip())
                         print(sender.send_notify_to_group('update', '정기점검 공지', element.text.strip()))
                 elif '임시' in title:
                     if '완료' in title:
-                        print('임시점검 완료: ', element.text.strip())
+                        print('(END)Temporary inspection: ', element.text.strip())
                         print(sender.send_notify_to_group('update', '임시점검 완료', element.text.strip()))
                     else:
-                        print('임시점검 예정: ', element.text.strip())
-                        print(sender.send_notify_to_group('update', '긴급 공지: 임시점검', element.text.strip()))
+                        print('Temporary inspection: ', element.text.strip())
+                        print(sender.send_notify_to_group('update', '임시점검 공지', element.text.strip()))
         before_update_list = copy.deepcopy(new_update_list)
 
-        # 신규 이벤트 확인
+        # Check new event
         page = requests.get("https://community.joycity.com/gw/event")
         soup = bs(page.text, "html.parser")
         elements = soup.select('div.sub_title a')
@@ -107,17 +107,17 @@ def check_community():
 
             if title not in before_event_list:
                 if '쿠폰' in title:
-                    print('이벤트(쿠폰): ', element.text.strip())
+                    print('Event(Coupon): ', element.text.strip())
                     print(sender.send_notify_to_group('event', '신규 이벤트 공지(쿠폰)', element.text.strip()))
                 elif '원스토어' in title:
-                    print('이벤트(원스토어): ', element.text.strip())
+                    print('Event(OneStore): ', element.text.strip())
                     print(sender.send_notify_to_group('event', '원스토어 이벤트', element.text.strip()))
                 else:
-                    print('이벤트: ', element.text.strip())
+                    print('Event: ', element.text.strip())
                     print(sender.send_notify_to_group('event', '신규 이벤트 공지', element.text.strip()))
         before_event_list = copy.deepcopy(new_event_list)
 
-        # 종료 이벤트 확인
+        # Check end event
         page = requests.get("https://community.joycity.com/gw/endEvent")
         soup = bs(page.text, "html.parser")
         elements = soup.select('div.sub_title a')
@@ -128,11 +128,11 @@ def check_community():
             new_end_event_list.append(title)
 
             if title not in before_end_event_list:
-                print('이벤트 종료: ', element.text.strip())
+                print('(End)Event: ', element.text.strip())
                 print(sender.send_notify_to_group('endEvent', '이벤트 종료', element.text.strip()))
         before_end_event_list = copy.deepcopy(new_end_event_list)
 
-        # 이벤트 당첨자 발표
+        # Check event winner
         page = requests.get("https://community.joycity.com/gw/eventResult")
         soup = bs(page.text, "html.parser")
         elements = soup.select('div.sub_title a')
@@ -143,11 +143,11 @@ def check_community():
             new_event_winner_list.append(title)
 
             if title not in before_event_winner_list:
-                print('이벤트 당첨자 발표: ', element.text.strip())
+                print('Event Winner: ', element.text.strip())
                 print(sender.send_notify_to_group('eventWinner', '이벤트 당첨자 발표', element.text.strip()))
         before_event_winner_list = copy.deepcopy(new_event_winner_list)
 
-        # 유튜브 신규 영상
+        # Check youtube video
         body = driver.find_element_by_tag_name('body')
         body.send_keys(Keys.PAGE_DOWN)
         time.sleep(1)
@@ -164,7 +164,9 @@ def check_community():
                 notification_title = "유튜브 신규 영상"
                 if '쿠폰' in video_title:
                     notification_title = notification_title + "(쿠폰)"
-                print(title, element.text.strip())
+                    print("Youtube Video(Coupon): ", element.text.strip())
+                else:
+                    print("Youtube Video: ", element.text.strip())
                 print(sender.send_notify_to_group('youtube', notification_title, element.text.strip()))
         before_youtube_list = copy.deepcopy(new_youtube_list)
 
