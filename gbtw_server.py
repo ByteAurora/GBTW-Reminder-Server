88762a9347e4f28a6a848aeca3377cb1bfdfd2fe
Sohn Young Jin <sonkim1001@naver.com>
2021-07-15 12:10:10 +0900

    Update main source - Remove thread logic

--- gbtw_server.py
+++ gbtw_server.py
@@ -2,5 +2,4 @@ import time
 import community_parser
 import threading
 
-check_community_trial_thread = threading.Thread(target=community_parser.check_community_trial())
-check_community_trial_thread.start()
+community_parser.check_community_trial()
